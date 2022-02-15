da54f007363a5f8c8631c4a189258b276caef74c
odedahay <odedahay@gmail.com>
2022-02-15 23:37:20 +0800

    added contact form and sharing validation

--- jobs/forms.py
+++ jobs/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class EmailJobForm(forms.Form):
     name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class':'height60', 'placeholder': 'First Name'}))
     # email = forms.EmailField()
-    to = forms.EmailField(widget=forms.TextInput(attrs={'class':'height60', 'placeholder':'sample@email.com'}))
+    to = forms.EmailField(widget=forms.EmailInput(attrs={'class':'height60', 'placeholder':'sample@email.com'}))
     comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'class':'textarea-height'}))
 
--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,11 @@
-# from django.http import JsonResponse
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from job_category.models import Category
 from .models import Job
 from .forms import EmailJobForm
 from django.core.mail import send_mail
 from django.conf import settings
-# from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse
 
 
 def jobs(request, category_slug=None):
@@ -39,31 +39,14 @@ def job_details(request, category_slug, job_slug):
         post_job = get_object_or_404(Job, slug=job_slug, is_available=True)
 
         # Share this job widget
-        sent = False
+        # sent = False
         
-        if request.method == 'POST':
-            # Form was submitted
-            form = EmailJobForm(request.POST)
-
-            if form.is_valid():
-                # Form fields passed validation
-                cd = form.cleaned_data
-                email_provider = settings.EMAIL_HOST_USER
-            
-                post_url = request.build_absolute_uri(post_job.get_url())
-                subject = f"{cd['name']} recommends you read " f"{post_job.job_name}"
-                message = f"AGILE CREW\'s job post for {post_job.job_name}, read more at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
-                send_mail(subject, message, email_provider, [cd['to']])
-
-                sent = True
-
-        else:
-
-            form = EmailJobForm()
-        # if is_ajax(request=request):
+        # if request.method == 'POST':
+        #     # Form was submitted
         #     form = EmailJobForm(request.POST)
 
         #     if form.is_valid():
+        #         # Form fields passed validation
         #         cd = form.cleaned_data
         #         email_provider = settings.EMAIL_HOST_USER
             
@@ -72,12 +55,29 @@ def job_details(request, category_slug, job_slug):
         #         message = f"AGILE CREW\'s job post for {post_job.job_name}, read more at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
         #         send_mail(subject, message, email_provider, [cd['to']])
 
-        #         return JsonResponse({
-        #             'msg':'Success'
-        #         })
-                
+        #         sent = True
+
         # else:
+
         #     form = EmailJobForm()
+        if is_ajax(request=request):
+            form = EmailJobForm(request.POST)
+
+            if form.is_valid():
+                cd = form.cleaned_data
+                email_provider = settings.EMAIL_HOST_USER
+            
+                post_url = request.build_absolute_uri(post_job.get_url())
+                subject = f"{cd['name']} recommends you read " f"{post_job.job_name}"
+                message = f"AGILE CREW\'s job post for {post_job.job_name}, read more at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+                send_mail(subject, message, email_provider, [cd['to']])
+
+                return JsonResponse({
+                    'msg':'Success'
+                })
+                
+        else:
+            form = EmailJobForm()
 
     except Exception as e:
         raise e
@@ -86,7 +86,6 @@ def job_details(request, category_slug, job_slug):
         'single_job': single_job,
         'post_job':post_job,
         'form': form,
-        'sent':sent
     }
 
     return render(request, 'jobs/job_details.html', context)
