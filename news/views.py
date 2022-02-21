4ce4adeaf733ad90b5c14d3e0849a6468c499903
odedahay <odedahay@gmail.com>
2022-02-22 04:40:26 +0800

    removed thinker

--- news/views.py
+++ news/views.py
@@ -1,5 +1,3 @@
-from ast import keyword
-from tkinter.messagebox import NO
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import News
